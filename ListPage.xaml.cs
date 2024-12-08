587e1f481f42a6d998217266b1d820b513262204
Dani Catarama <cataramadani03@gmail.com>
2024-12-08 11:11:15 +0200

    Laborator  9

--- ListPage.xaml.cs
+++ ListPage.xaml.cs
@@ -1,11 +1,31 @@
-namespace CataramaAlexandruLab7;
 using CataramaAlexandruLab7.Models;
+namespace CataramaAlexandruLab7;
 public partial class ListPage : ContentPage
 {
 	public ListPage()
 	{
 		InitializeComponent();
 	}
-    async void OnSaveButtonClicked(object sender, EventArgs e) { var slist = (ShopList)BindingContext; slist.Date = DateTime.UtcNow; await App.Database.SaveShopListAsync(slist); await Navigation.PopAsync(); }
-    async void OnDeleteButtonClicked(object sender, EventArgs e) { var slist = (ShopList)BindingContext; await App.Database.DeleteShopListAsync(slist); await Navigation.PopAsync(); }
+    async void OnSaveButtonClicked(object sender, EventArgs e) {
+     var slist = (ShopList)BindingContext; slist.Date = DateTime.UtcNow;
+    await App.Database.SaveShopListAsync(slist);
+    await Navigation.PopAsync(); 
+    }
+    protected override async void OnAppearing() 
+    {
+        base.OnAppearing();
+        var shopl = (ShopList)BindingContext;
+        listView.ItemsSource = await App.Database.GetListProductsAsync(shopl.ID); 
+    }
+    async void OnDeleteButtonClicked(object sender, EventArgs e)
+    { 
+        var slist = (ShopList)BindingContext; await App.Database.DeleteShopListAsync(slist); await Navigation.PopAsync();
+    async void OnChooseButtonClicked(object sender, EventArgs e)
+     {
+     await Navigation.PushAsync(new ProductPage((ShopList)this.BindingContext)
+            {
+                BindingContext = new Product()
+            });
+     }
+    }
 }
